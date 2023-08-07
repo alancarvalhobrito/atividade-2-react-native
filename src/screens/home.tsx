affdee357f6c4d7baf51b52f26b03dfd7296a3bd
alcarvalho <alcarvalho@lopes.com.br>
2023-08-07 07:31:53 -0300

    Commit inicial do projeto com navegação

---
+++ src/screens/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <SafeAreaView/>
+            <Text>ESTOU NA HOME, DEU BOM!</Text>
+        </View>
+    )
+}
+
+export default Home;
